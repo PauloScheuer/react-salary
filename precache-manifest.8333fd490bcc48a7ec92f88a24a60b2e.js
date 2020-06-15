afc341429ded4c8624e916a325d618f8de01d2f2
PauloScheuer <PauloScheuer@users.noreply.github.com>
2020-06-15 12:43:27 -0300

    Updates

---
+++ precache-manifest.8333fd490bcc48a7ec92f88a24a60b2e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4e05350aa3aaf37590c60d41e7e87a6",
+    "url": "/react-salary/index.html"
+  },
+  {
+    "revision": "9fd9a3fd829a01f703fa",
+    "url": "/react-salary/static/css/main.d2b766d7.chunk.css"
+  },
+  {
+    "revision": "8267c7eecf65a6433b43",
+    "url": "/react-salary/static/js/2.419ae365.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-salary/static/js/2.419ae365.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9fd9a3fd829a01f703fa",
+    "url": "/react-salary/static/js/main.fd8c2919.chunk.js"
+  },
+  {
+    "revision": "c557c2d8db103039294a",
+    "url": "/react-salary/static/js/runtime-main.f1052e33.js"
+  }
+]);
